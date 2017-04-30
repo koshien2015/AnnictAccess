64098eca177215a4e04fc230ca4b321a2db7e1e0
ckoshien <ckoshien@gmail.com>
2017-04-30 12:34:07 +0900

    修正

--- AnnictAccess/src/main/java/cx/myhome/ckoshien/annict/action/IndexAction.java
+++ AnnictAccess/src/main/java/cx/myhome/ckoshien/annict/action/IndexAction.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2004-2008 the Seasar Foundation and the Others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
 package cx.myhome.ckoshien.annict.action;
 
 import java.net.InetAddress;
@@ -112,6 +97,7 @@ public class IndexAction {
 			e1.printStackTrace();
 		}
     	if(loginDto.getAccess_token()==null){
+    		logger.info("401");
     		return "401.jsp";
     	}
     	String uri="https://api.annict.com/v1/me/programs?sort_started_at=asc&per_page=40&filter_unwatched=true&accessToken="+loginDto.getAccess_token();
